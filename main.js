4f80873b0b951da0d075730be63f282def958814
DaliaZegaya <daliazegaya56@gmail.com>
2022-05-25 13:51:28 +0300

    commit

---
+++ main.js
@@ -0,0 +1,80 @@
+// var Boss= {
+//     firstName: "Dalia",
+//     lastName: "Zegaya",
+//     age: 23,
+//     wages: "50,000$"
+// }
+
+// var employee= {
+//     firstName: "Batel",
+//     lastName: "Haiylo",
+//     YearofBirth: 1997,
+//     email: "batel12@gmail.com",
+//     adress: "anna frank 4, lod"
+// }
+
+// var fruit= {
+//     name: "watermelon",
+//     color: "red",
+//     type: "summer"
+// }
+
+// document.write (fruit.name)
+// document.write (fruit.color)
+// document.write (fruit.type)
+
+// var classRoom= {
+//     numberClass: 5,
+//     numberOfStudents: 5,
+//     nameOfStudents: ["batel", "chen", "or", "yonatan", "daniel"]
+// }
+// for (var i=0; i<classRoom.nameOfStudents.length;i++) {
+//     document.write (classRoom.nameOfStudents[i])
+// }
+
+// var kinderGarden= {
+//     KindergartenTeacher: "shosi",
+//     numberOfkids: 10,
+//     namesOfkids: ["batel","yosi","yonatan","chen","netanel","daniel","solomon","sara","lior","aviel"]
+// }
+// for (var i=0; i<kinderGarden.namesOfkids.length;i++) {
+//     document.write (kinderGarden["namesOfkids"][i])
+// }
+// var myKey= prompt("enter")
+// document.write (kinderGarden[myKey])
+
+// var course= {}  
+// course.courseNumber= +prompt("course")
+// course.numberOfstudents= prompt("number")
+// course.age= +prompt("your age")
+
+// console.log(course);
+
+// var someObject= {}
+// var keyUser= prompt("key")
+// var valueUser= prompt("value")
+// someObject[keyUser]=valueUser
+// console.log(someObject);
+
+// var teacher= {}
+// var keyName= prompt ("key")
+// var valueUser= prompt ("value")
+// teacher[keyName]= valueUser
+// console.log(teacher);
+
+// var directorLongWay= {}
+// var keyName= prompt("name")
+// var keyLastName= prompt("last name")
+// var keyAge= prompt("age")
+// var keyWages= prompt("wages")
+// director[keyName]=prompt("enter your name")
+// director[keyLastName]=prompt("enter your last name")
+// director[keyAge]=+prompt("enter your age")
+// director[keyWages]=prompt("enter your wages")
+// console.log(director);
+
+// var directorShortWay= {}
+// for (var i=0; i<4;i++) {
+//     director[prompt("key")]=prompt("value")
+// }
+// console.log(director);
